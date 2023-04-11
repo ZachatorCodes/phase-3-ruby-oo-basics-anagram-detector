809a84472bc916cba6a41f0267edfd580b75be7b
Zach Schwartz <zachatorcodes@gmail.com>
2023-04-11 14:46:00 -0600

    finished lab

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
-# Your code goes here!
+class Anagram
+  def initialize(word)
+    @word = word
+  end
+
+  def match(word_array)
+    word_array.filter do |word|
+      word.chars.sort == @word.chars.sort
+    end
+  end
+end
